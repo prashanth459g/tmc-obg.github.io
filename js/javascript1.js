93528908b209afa9c2624aa7d6fd312dc428bcf6
prashanth459g <90688113+prashanth459g@users.noreply.github.com>
2022-04-10 16:55:44 +0530

    new case JS

--- js/javascript1.js
+++ js/javascript1.js
@@ -3,3 +3,16 @@ function closeme() {
   x.style.display = "none";
 
 }
+
+var cases = [ ];
+// document.getElementById("cases").innerHTML = cases;
+
+function loadcases() {
+  document.getElementById("cases").innerHTML = cases;
+}
+
+function newcase() {
+  var case1 = prompt("Enter Details of new case");
+  cases[cases.length] = "<br>" + case1 ;
+  document.getElementById("cases").innerHTML = cases;
+}
